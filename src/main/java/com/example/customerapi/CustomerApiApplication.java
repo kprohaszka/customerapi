3509e80742f89f9a04d1c589e7cd2f08da63d351
Konrad Prohaszka <cc.kprohaszka@gmail.com>
2025-03-02 14:08:13 +0100

    Create secret key generation for Jwt token and arrange how it is being saved

--- src/main/java/com/example/customerapi/CustomerApiApplication.java
+++ src/main/java/com/example/customerapi/CustomerApiApplication.java
@@ -1,5 +1,6 @@
 package com.example.customerapi;
 
+import com.example.customerapi.utils.KeyGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CustomerApiApplication {
 
 	public static void main(String[] args) {
+		String secretKey = KeyGenerator.generateSecretKey();
+		System.setProperty("JWT_SECRET_KEY", secretKey);
 		SpringApplication.run(CustomerApiApplication.class, args);
 	}
 
